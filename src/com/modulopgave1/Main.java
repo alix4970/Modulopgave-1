2d3d7fc23c27036dd9c903948ed275c1d63ff528
dani153i <danielblom90@gmail.com>
2019-03-01 14:14:51 +0100

    update

--- src/com/modulopgave1/Main.java
+++ src/com/modulopgave1/Main.java
@@ -1,20 +1,58 @@
 package com.modulopgave1;
 
+import java.util.Scanner;
+
+import com.modulopgave1.util.Console;
 import com.modulopgave1.dal.MigrationRepository;
 import com.modulopgave1.dal.PeopleStatisticRepository;
 import com.modulopgave1.ui.statistics.migrations.MigrationsController;
 import com.modulopgave1.ui.statistics.peoplestatistic.PeopleStatisticController;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    static PeopleStatisticController peoplestatistcController;
+    static MigrationsController migrationsController;
+
 
     public static void main(String[] args)
     {
         PeopleStatisticRepository peoplestatisticRepo = new PeopleStatisticRepository();
-        PeopleStatisticController peoplestatistcController = new PeopleStatisticController(peoplestatisticRepo);
-        peoplestatistcController.displayMovementView();
+        peoplestatistcController = new PeopleStatisticController(peoplestatisticRepo);
 
         MigrationRepository migrationRepo = new MigrationRepository();
-        MigrationsController migrationsController = new MigrationsController(migrationRepo);
-        migrationsController.displayMigrationsView();
+        migrationsController = new MigrationsController(migrationRepo);
+
+        displayMainView();
+    }
+
+    public static void displayMainView() {
+        while(true) {
+            Console.clearConsole();
+            System.out.println("MAIN MENU\n");
+            System.out.println("1) Migrations");
+            System.out.println("2) People Statistics");
+            System.out.println("\n0) Back");
+
+            int input = -1;
+            while (!((input == 0) || (input >= 1 && input <= 2))) {
+                System.out.print("input\t>>\t");
+                try {
+                    input = Integer.parseInt(scanner.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("input must be an integer..");
+                }
+            }
+
+            switch (input) {
+                case 0:
+                    return;
+                case 1:
+                    migrationsController.displayMigrationsView();
+                    break;
+                case 2:
+                    peoplestatistcController.displayMovementView();
+                    break;
+            }
+        }
     }
 }
